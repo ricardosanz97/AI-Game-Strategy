cb8eb589587d3c782e8323b82e35f3e91b30c0d7
Ricardo Sanz Ferrero <rferrero@ontinet.com>
2018-12-08 12:06:13 +0100

    Popup generico creado para lso dos botones. Para probarlo, spawnead un enemigo.

---
+++ Assets/SimpleOptionsPopupController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class SimpleOptionsPopupController : PopupController {
+    public Text textPopup;
+    public Button button1;
+    public Button button2;
+
+    public void SetPopup(string titleText, string button1Text, string button2Text, UnityAction button1Action, UnityAction button2Action)
+    {
+        this.transform.SetParent(GameObject.Find("Canvas").transform);
+        this.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        OpenPopup();
+        this.textPopup.text = titleText;
+        this.button1.GetComponentInChildren<Text>().text = button1Text;
+        this.button2.GetComponentInChildren<Text>().text = button2Text;
+        button1.onClick.AddListener(button1Action);
+        button2.onClick.AddListener(button2Action);
+    }
+}
