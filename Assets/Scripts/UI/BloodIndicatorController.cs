ce26baba11fe520438d57ad2d85460227511f6e2
oskarter9 <oscar5teruel@gmail.com>
2018-12-13 22:41:46 +0100

    cosas guapas hechas

--- Assets/Scripts/UI/BloodIndicatorController.cs
+++ Assets/Scripts/UI/BloodIndicatorController.cs
@@ -5,54 +5,55 @@ using UnityEngine.UI;
 
 public class BloodIndicatorController : MonoBehaviour {
 
-    private Slider bloodIndicator;
+    private Image bloodIndicator;
     private Text bloodValueText;
     private int minValue = 0;
     private int maxValue = 10;
 
     private void Awake()
     {
-        bloodIndicator = GameObject.FindGameObjectWithTag("BloodIndicator").GetComponent<Slider>();
+        bloodIndicator = GameObject.FindGameObjectWithTag("BloodIndicator").GetComponent<Image>();
         bloodValueText = bloodIndicator.GetComponentInChildren<Text>();
     }
 
     private void Start()
     {
-        bloodIndicator.value = maxValue;
-        bloodValueText.text = bloodIndicator.value.ToString();
+        bloodIndicator.fillAmount = 1f;
+        bloodValueText.text = ((int)(bloodIndicator.fillAmount * maxValue)).ToString();
     }
 
     public void DecreaseBloodValue (int value)
     {
-        if ((int)bloodIndicator.value - value > minValue)
+        float value2 = value / 10f;
+
+        if (bloodIndicator.fillAmount - value2 > minValue)
         {
-            bloodIndicator.value -= value;
-            bloodValueText.text = bloodIndicator.value.ToString();
+            bloodIndicator.fillAmount -= value2;
         }
         else
         {
-            bloodIndicator.value = minValue;
-            bloodValueText.text = bloodIndicator.value.ToString();
+            bloodIndicator.fillAmount = minValue;
         }
+        bloodValueText.text = ((int)Mathf.Round(bloodIndicator.fillAmount * maxValue)).ToString();
     }
 
     public void IncreaseBloodValue (int value)
     {
-        if ((int)bloodIndicator.value + value < maxValue)
+        float value2 = value / 10f;
+
+        if (bloodIndicator.fillAmount + value2 < maxValue)
         {
-            bloodIndicator.value += value;
-            bloodValueText.text = bloodIndicator.value.ToString();
+            bloodIndicator.fillAmount += value2;
         }
         else
         {
-            bloodIndicator.value = maxValue;
-            bloodValueText.text = bloodIndicator.value.ToString();
+            bloodIndicator.fillAmount = 1f;
         }
+        bloodValueText.text = ((int)Mathf.Round(bloodIndicator.fillAmount * maxValue)).ToString();
     }
 
     public int GetCurrentBlood()
     {
-        return (int)bloodIndicator.value;
+        return (int)Mathf.Round(bloodIndicator.fillAmount * maxValue);
     }
-
 }
