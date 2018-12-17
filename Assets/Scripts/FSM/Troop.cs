00d611f956157edd52d9fea0cf2861815ccab0d5
Ricardo <rferrero@ontinet.com>
2018-12-18 00:52:27 +0100

    moviimiento en mi misma x

--- Assets/Scripts/FSM/Troop.cs
+++ Assets/Scripts/FSM/Troop.cs
@@ -158,7 +158,7 @@ public class Troop : AbstractNPCBrain
             Debug.Log("generando lista de posibles movimientos. ");
             if (this.owner == Owner.Player)
             {
-                if (this.cell.PNode.GridX < node.GridX)
+                if (this.cell.PNode.GridX <= node.GridX)
                 {
                     node.cell.gameObject.transform.Find("MovePlacement").gameObject.SetActive(true);
                     possibleMovements.Add(node);
@@ -166,7 +166,7 @@ public class Troop : AbstractNPCBrain
             }
             else
             {
-                if (this.cell.PNode.GridX > node.GridX)
+                if (this.cell.PNode.GridX >= node.GridX)
                 {
                     node.cell.gameObject.transform.Find("MovePlacement").gameObject.SetActive(true);
                     possibleMovements.Add(node);
