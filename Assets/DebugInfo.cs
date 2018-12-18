d63106496b32995376029dd7bf471df11d9b15f5
Eduardo Simón Picón <al341513@uji.es>
2018-12-18 14:13:46 +0100

    Añadido el debug

--- Assets/DebugInfo.cs
+++ Assets/DebugInfo.cs
@@ -2,21 +2,33 @@
 using System.Collections.Generic;
 using CustomPathfinding;
 using InfluenceMap;
+using ModestTree;
 using UnityEngine;
+using UnityEngine.UI;
+using Assert = UnityEngine.Assertions.Assert;
 
 public class DebugInfo : MonoBehaviour
 {
 	
 	private InfluenceMapComponent _influenceMapComponent;
 	private PathfindingGrid _pathfindingGrid;
+	private LevelController _levelController;
+	private RawImage _influenceTexture;
 	
 	private bool activated = true;	
 	
 	// Use this for initialization
 	private void Awake()
 	{
-		_influenceMapComponent = GetComponent<InfluenceMapComponent>();
+		_influenceMapComponent = FindObjectOfType<InfluenceMapComponent>();
+		_levelController = FindObjectOfType<LevelController>();
 		_pathfindingGrid = FindObjectOfType<PathfindingGrid>();
+		_influenceTexture = GameObject.FindGameObjectWithTag("InfluenceTexture").GetComponent<RawImage>();
+		
+		Assert.IsNotNull(_influenceTexture);
+		Assert.IsNotNull(_influenceMapComponent);
+		Assert.IsNotNull(_levelController);
+		Assert.IsNotNull(_pathfindingGrid);	
 	}
 	
 	private void OnGUI()
@@ -28,6 +40,13 @@ public class DebugInfo : MonoBehaviour
 			_influenceMapComponent.influenceGrid._renderGroundGrid = activated;
 			_influenceMapComponent.influenceGrid.UpdateMap();
 			_pathfindingGrid.ChangeNodeDisplayMode(activated);
+
+			foreach (var entity in _levelController.TotalEntities)
+			{
+				entity.GetComponent<AbstractNPCBrain>().currentStateDebug.enabled = activated;
+			}
+			
+			_influenceTexture.enabled = activated;
 		}
 		
 	}
